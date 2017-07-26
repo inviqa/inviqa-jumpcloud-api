fd141380db5f2ec77c75d2560e4b9175f47c47b7
Andy Thompson <athompson@inviqa.com>
2017-07-26 10:39:43 +0100

    Add SystemUsers systems resource

---
+++ src/Console/Resource/AbstractBinding.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Inviqa\JumpCloud\Api\Console\Resource;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Psr7\Uri;
+
+abstract class AbstractBinding
+{
+    /**
+     * @var string
+     */
+    protected $endpoint = null;
+
+    /**
+     * @var GuzzleClient
+     */
+    protected $client;
+
+    public function __construct(GuzzleClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getName()
+    {
+        return $this->endpoint;
+    }
+
+    protected function formatUri($resource_id, $options = [])
+    {
+        $uri = new Uri(sprintf($this->endpoint, $resource_id));
+        foreach (['limit', 'skip', 'sort', 'fields'] as $key) {
+            if (array_key_exists($key, $options)) {
+                $uri = Uri::withQueryValue($uri, $key, $options[$key]);
+            }
+        }
+        return $uri;
+    }
+
+    public function fetch($resource_id, $options = [])
+    {
+        $response = $this->client->get($this->formatUri($resource_id, $options));
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $result;
+    }
+
+    public function put($resource_id, $data)
+    {
+        $response = $this->client->put(
+            $this->formatUri($resource_id),
+            [
+                'json' => $data
+            ]
+        );
+        return json_decode($response->getBody()->getContents(), true);
+    }
+}
